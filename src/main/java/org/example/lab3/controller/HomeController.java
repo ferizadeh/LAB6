0ed1b1cb87c18fc4b24e770c9e642a7819a6f0c4
ferizadeh <farzadalizadeh1982@gmail.com>
2025-10-06 22:18:42 -0400

    commit

---
+++ src/main/java/org/example/lab3/controller/HomeController.java
@@ -0,0 +1,13 @@
+package org.example.lab3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Address Book API is running! Try /api/addressbooks or use a REST client like Postman.";
+    }
+}
